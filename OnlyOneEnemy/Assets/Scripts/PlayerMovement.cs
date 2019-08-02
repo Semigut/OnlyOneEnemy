52f27948c86f552aa7086b4acca948e195c5db58
IchHalt <firen8@hotmail.de>
2019-08-03 00:15:42 +0200

    Animation
    4 Way Animation Idle and walk

--- OnlyOneEnemy/Assets/Scripts/PlayerMovement.cs
+++ OnlyOneEnemy/Assets/Scripts/PlayerMovement.cs
@@ -8,11 +8,15 @@ public class PlayerMovement : MonoBehaviour
     public float speed;
     private Rigidbody2D myRigidbody;
     private Vector3 change;
-    
+    private Animator animator;
+
+
     // Start is called before the first frame update
     void Start()
     {
+        animator = GetComponent<Animator>();
         myRigidbody = GetComponent<Rigidbody2D>();
+
     }
 
     // Update is called once per frame
@@ -22,11 +26,24 @@ public class PlayerMovement : MonoBehaviour
         change = Vector2.zero;
         change.x = Input.GetAxisRaw("Horizontal");
         change.y = Input.GetAxisRaw("Vertical");
+        UpdateAnimationAndMove();
+
+    }
+
+    void UpdateAnimationAndMove()
+    {
         if (change != Vector3.zero)
         {
             MoveCharacter();
-        }
+            animator.SetFloat("moveX", change.x);
+            animator.SetFloat("moveY", change.y);
+            animator.SetBool("moving", true);
 
+        }
+        else
+        {
+            animator.SetBool("moving", false);
+        }
     }
 
     void MoveCharacter ()
