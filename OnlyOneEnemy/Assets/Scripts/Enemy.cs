7222f3dbf9769fcbd266b0215b7c37fe76b70e6e
IchHalt <firen8@hotmail.de>
2019-08-03 12:06:50 +0200

    EnemyScript

---
+++ OnlyOneEnemy/Assets/Scripts/Enemy.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+
+    public Animator anim;
+
+    public bool change = false;
+    // Start is called before the first frame update
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+
+    }
+
+    public void Smash()
+    {
+        anim.SetBool("smash", true);
+     
+
+
+    }
+
+    public void UnSmash()
+    {
+     anim.SetBool("smash", false);
+           
+
+    }
+
+}
